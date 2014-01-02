73048143765226d4626b26736932e88e4cbe9c11
js <js@js-X58A-UD3R.(none)>
2014-01-03 00:46:34 +0100

    can write on screen

--- opengl/Bindings.hs
+++ opengl/Bindings.hs
@@ -11,7 +11,8 @@ reshape size = do
 
 keyboardMouse :: IORef String -> IORef GLfloat -> IORef (GLfloat, GLfloat) -> KeyboardMouseCallback
 keyboardMouse s a p key Down _ _ = case key of
-  (Char ' ') -> a $~! negate
+  --(Char ' ') -> a $~! negate
+  (Char '\b') -> s $~! \(x:xs) -> xs--writeIORef s (x:s')
   (Char '+') -> a $~! (*2)
   (Char '-') -> a $~! (/ 2)
   (Char x) -> s $~! \str -> (x:str)--writeIORef s (x:s')
--- opengl/Display.hs
+++ opengl/Display.hs
@@ -14,11 +14,11 @@ vert x y z = Vertex3 (fromIntegral x) (fromIntegral y) (realToFrac z)
 setCamera xtarget ztarget = do
   --matrixMode $= Projection
   --loadIdentity
-  lookAt (Vertex3 (10+xtarget) 10 ((10+ztarget)::Double)) (Vertex3 xtarget 0 (ztarget::Double)) (Vector3 0 1 (0::Double))
+  lookAt (Vertex3 0 0 (50::Double)) (Vertex3 0 0 (0::Double)) (Vector3 0 1 (0::Double))
 
 display :: IORef String -> IORef GLfloat -> IORef (GLfloat, GLfloat) -> IORef [(GLfloat, GLfloat)] -> DisplayCallback
 display str angle pos units = do
-  --curStr <- get str
+  curStr <- get str
   --putStrLn curStr  
   clear [ColorBuffer, DepthBuffer]
   matrixMode $= Modelview 0
@@ -28,17 +28,19 @@ display str angle pos units = do
       boxes = [((x::GLfloat), 0, z) | x <- [0..width], z <- [0..width]]
 
   preservingMatrix $ do
+    color (Color3 1 1 (0::GLfloat))
+    scale 0.01 0.01 (0.01::GLfloat)
+    translate $ Vector3 (-100) 30 (-100::GLfloat)
+    renderString Roman $ reverse curStr
     color (Color3 1 0 (0::GLfloat))
-    scale 0.001 0.001 (0.001::GLfloat)
-    renderString Roman "Test string"
     scale 1 1 (1::GLfloat)
     units' <- get units
     forM_ units' $ \(x, z) -> preservingMatrix $ do
       loadIdentity
       translate $ Vector3 (x*2) 2 (z*2)
       color (Color3 1 0 (0::GLfloat))
-      cube 0.8
-      color (Color3 0 0 (0::GLfloat))
+      --cube 0.8
+      --color (Color3 0 0 (0::GLfloat))
       cubeFrame 0.8
 
   preservingMatrix $ do
@@ -47,11 +49,9 @@ display str angle pos units = do
       loadIdentity
       color $ Color3 ((x+1)/2) ((y+1)/2) ((z+1)/2)
       translate $ Vector3 (x*2) 0 (z*2)
-      cube 1
-      color $ Color3 (0 ::GLfloat) 0 0 
+      --cube 1
+      --color $ Color3 (0 ::GLfloat) 0 0 
       cubeFrame 1
-    cube 1
-    cubeFrame 1
   swapBuffers
 
 idle :: IORef GLfloat -> IORef GLfloat -> IdleCallback
