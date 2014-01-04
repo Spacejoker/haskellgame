fb83be08376fb1bd623c3e2c4e188194275953af
js <js@js-X58A-UD3R.(none)>
2014-01-05 00:04:40 +0100

    Working texture-example from bmp

--- test-texture/main.hs
+++ test-texture/main.hs
@@ -13,9 +13,19 @@ import Data.Bits ( (.|.) )
 import System.Exit ( exitWith, ExitCode(..) )
 import Control.Monad ( forever )
 import Data.IORef ( IORef, newIORef, readIORef, writeIORef )
+import Foreign ( withForeignPtr, plusPtr, peek, alloca )
+import qualified Data.ByteString.Internal as BSI
+import Data.Maybe
+--import Codec.Picture
+--import Paths_nehe_tuts
 
-initGL :: GLFW.Window -> IO ()
+import Util
+--import Cube
+
+
+initGL :: GLFW.Window -> IO GLuint
 initGL win = do
+  glEnable gl_TEXTURE_2D
   glShadeModel gl_SMOOTH -- enables smooth color shading
   glClearColor 0 0 0 0 -- Clear the background color to black
   glClearDepth 1 -- enables clearing of the depth buffer
@@ -24,6 +34,30 @@ initGL win = do
   glHint gl_PERSPECTIVE_CORRECTION_HINT gl_NICEST
   (w,h) <- GLFW.getFramebufferSize win
   resizeScene win w h
+  --return (1::GLuint)
+  loadGLTextures
+
+loadGLTextures :: IO GLuint
+loadGLTextures = do
+  fp <- getDataFileName "tex.bmp"
+  putStrLn $ "loading texture: " ++ fp
+  Just (Image w h pd) <- bitmapLoad fp
+  putStrLn $ "Image width  = " ++ show w
+  putStrLn $ "Image height = " ++ show h
+  tex <- alloca $ \p -> do
+    glGenTextures 1 p
+    peek p
+  let (ptr, off, _) = BSI.toForeignPtr pd
+  withForeignPtr ptr $ \p -> do
+    let p' = p `plusPtr` off
+    glBindTexture gl_TEXTURE_2D tex
+    glTexImage2D gl_TEXTURE_2D 0 3
+      (fromIntegral w) (fromIntegral h) 0 gl_RGB gl_UNSIGNED_BYTE
+      p'
+    let glLinear = fromIntegral gl_LINEAR
+    glTexParameteri gl_TEXTURE_2D gl_TEXTURE_MIN_FILTER glLinear
+    glTexParameteri gl_TEXTURE_2D gl_TEXTURE_MAG_FILTER glLinear
+  return tex
 
 resizeScene :: GLFW.WindowSizeCallback
 resizeScene win w     0      = resizeScene win w 1 -- prevent divide by zero
@@ -36,86 +70,87 @@ resizeScene _   width height = do
   glLoadIdentity
   glFlush
 
-drawScene :: IORef GLfloat -> GLFW.Window  -> IO ()
-drawScene angle _  = do
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  return ("/home/js/dev/haskell/rpg/test-texture/textures/" ++ name)
+
+drawScene :: GLuint -> IORef GLfloat -> GLFW.Window  -> IO ()
+drawScene tex angle _  = do
   -- clear the screen and the depth buffer
   glClear $ fromIntegral  $  gl_COLOR_BUFFER_BIT
                          .|. gl_DEPTH_BUFFER_BIT
   glLoadIdentity  -- reset view
-  glTranslatef 0 0 (-6.0) --Move left 1.5 Units and into the screen 6.0
+  glTranslatef 0 0 (-6.0) 
 
-  angle_ <- readIORef angle
-  --putStrLn angle
-  glRotatef angle_ 0 0 1
-  glBegin gl_TRIANGLES
-  glColor3f 1 0 0
-  glVertex3f 0 1 0
-  glColor3f 1 0 1
-  glVertex3f (-1) (-1) 0
-  glColor3f 1 1 0
-  glVertex3f 1 (-1) 0
-  glEnd
+  glRotatef 50 1 0 0
+  glRotatef 25 0 2 0
 
-  glLoadIdentity
-  glTranslatef 0 0 (-5.0) --Move left 1.5 Units and into the screen 6.0
-
-  glRotatef (angle_*20) 1 0.3 0.5
-  glRotatef (angle_) 0 1 0
-  -- cube
-  let w = 1
-  glBegin gl_QUADS
-
-  glColor3f 1 0.5 0
-
-  glVertex3f 0 0 0
-  glVertex3f w 0 0
-  glVertex3f w w 0
-  glVertex3f 0 w 0
-
-  glColor3f 1 0 0
-
-  glVertex3f 0 0 0
-  glVertex3f 0 0 w
-  glVertex3f 0 w w
-  glVertex3f 0 w 0
-
-  glColor3f 0 0 1
-
-  glVertex3f w 0 0
-  glVertex3f w 0 w
-  glVertex3f w w w
-  glVertex3f w w 0
-
-  glColor3f 0 0.5 0
-
-  glVertex3f 0 0 w
-  glVertex3f w 0 w
-  glVertex3f w w w
-  glVertex3f 0 w w
-
-  glColor3f 0 0.5 1
-
-  glVertex3f 0 0 0
-  glVertex3f w 0 0
-  glVertex3f w 0 w
-  glVertex3f 0 0 w
-
-  glColor3f 1 0.5 1
-
-  glVertex3f 0 w 0
-  glVertex3f w w 0
-  glVertex3f w w w
-  glVertex3f 0 w w
-
-  glEnd
-
-  
-
-  writeIORef angle  $! angle_ + 0.1
+  cube 1 (Just tex)
+  --writeIORef angle  $! angle_ + 0.1
   --angle $~! \angle -> (angle + 0.1)
 
   glFlush
 
+cube :: GLfloat -> Maybe GLuint -> IO()
+cube w tex = do
+  glBindTexture gl_TEXTURE_2D (fromJust tex)
+
+  glBegin gl_QUADS -- start drawing a polygon (4 sided)
+  glTexCoord2f   0    0
+  glVertex3f   (-w) (-w)    w  -- bottom left of quad (Front)
+  glTexCoord2f   1    0
+  glVertex3f     w  (-w)    w  -- bottom right of quad (Front)
+  glTexCoord2f   1    1 
+  glVertex3f     w    w     w  -- top right of quad (Front)
+  glTexCoord2f   0    1 
+  glVertex3f   (-w)   w     w  -- top left of quad (Front)
+  -- now the back
+  glTexCoord2f   1    0 
+  glVertex3f   (-w) (-w) (-w)  -- bottom right of quad (Back)
+  glTexCoord2f   1    1 
+  glVertex3f   (-w)   w  (-w)  -- top right of quad (Back)
+  glTexCoord2f   0    1 
+  glVertex3f     w    w  (-w)  -- top left of quad (Back)
+  glTexCoord2f   0    0 
+  glVertex3f     w  (-w) (-w)  -- bottom left of quad (Back)
+  -- now the top
+  glTexCoord2f   0    1
+  glVertex3f   (-w)   w  (-w)  -- top left of quad (Top)
+  glTexCoord2f   0    0  
+  glVertex3f   (-w)   w    w   -- bottom left of quad (Top)
+  glTexCoord2f   1    0  
+  glVertex3f     w    w    w   -- bottom right of quad (Top)
+  glTexCoord2f   1    1  
+  glVertex3f     w    w  (-w)  -- top right of quad (Top)
+  -- now the bottom
+  glTexCoord2f   1    1  
+  glVertex3f     w  (-w)   w   -- top right of quad (Bottom)
+  glTexCoord2f   0    1  
+  glVertex3f   (-w) (-w)   w   -- top left of quad (Bottom)
+  glTexCoord2f   0    0 
+  glVertex3f   (-w) (-w) (-w)  -- bottom left of quad (Bottom)
+  glTexCoord2f   1    0  
+  glVertex3f     w  (-w) (-w)  -- bottom right of quad (Bottom)
+  -- now the right
+  glTexCoord2f   1    0  
+  glVertex3f     w  (-w) (-w)  -- bottom right of quad (Right)
+  glTexCoord2f   1    1  
+  glVertex3f     w    w  (-w)  -- top right of quad (Right)
+  glTexCoord2f   0    1  
+  glVertex3f     w    w    w   -- top left of quad (Right)
+  glTexCoord2f   0    0  
+  glVertex3f     w  (-w)   w   -- bottom left of quad (Right)
+  -- now the left
+  glTexCoord2f   0    0  
+  glVertex3f   (-w) (-w) (-w)  -- bottom left of quad (Left)
+  glTexCoord2f   1    0  
+  glVertex3f   (-w)   w  (-w)  -- top left of quad (Left)
+  glTexCoord2f   1    1  
+  glVertex3f   (-w)   w    w   -- top right of quad (Left)
+  glTexCoord2f   0    1 
+  glVertex3f   (-w) (-w)   w   -- bottom right of quad (Left)
+  
+  glEnd
 shutdown :: GLFW.WindowCloseCallback
 shutdown win = do
   GLFW.destroyWindow win
@@ -130,6 +165,7 @@ keyPressed _   _               _ _                     _ = return ()
 main :: IO ()
 main = do
      True <- GLFW.init
+     --loadImage
      GLFW.defaultWindowHints
      -- get a 640 x 480 window
      -- initialize our window.
@@ -137,16 +173,18 @@ main = do
      GLFW.makeContextCurrent (Just win)
      -- register the function to do all our OpenGL drawing
      angle <- newIORef 0
-     GLFW.setWindowRefreshCallback win (Just (drawScene angle))
+     tex <- initGL win
+     putStrLn $ show tex
+     GLFW.setWindowRefreshCallback win (Just (drawScene tex angle))
      -- register the funciton called when our window is resized
      GLFW.setFramebufferSizeCallback win (Just resizeScene)
      -- register the function called when the keyboard is pressed.
      GLFW.setKeyCallback win (Just keyPressed)
      -- register window close handler
      GLFW.setWindowCloseCallback win (Just shutdown)
-     initGL win
      -- start event processing engine
      forever $ do
        GLFW.pollEvents
-       drawScene angle win
+       drawScene tex angle win
        GLFW.swapBuffers win
+
